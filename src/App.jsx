155bb2e11595ee184dc6bdd8ac234d7ee94c70ea
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-03-08 15:39:47 +0300

    add JSON placeholder enpoint '/todos'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from 'react'
+
+import s from './style.module.css'
+
 function App() {
+	const [isLoading, setIsLoading] = useState(true)
+	const [todoLists, setTodoLists] = useState([])
+
+	useEffect(() => {
+		document.body.classList.add('light')
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((response) => response.json())
+			.then((data) => {
+				setTodoLists(data)
+			})
+			.finally(() => setIsLoading(false))
+	}, [])
+
 	return (
 		<>
-			<h1>ToDo List Application</h1>
+			<div className={s.container}>
+				<div className={s.header}>
+					<h1>Список дел</h1>
+				</div>
+				{isLoading ? (
+					<div className={s.loader}></div>
+				) : (
+					<div className={s.content}>
+						{todoLists.map((todo, index) => (
+							<div key={index} className={s.list}>
+								{todo.title}
+							</div>
+						))}
+					</div>
+				)}
+			</div>
 		</>
 	)
 }
